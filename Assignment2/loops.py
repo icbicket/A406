10930c16d1bb4cbafc06310e375097b80579b805
banana-cultivator <45516370+banana-cultivator@users.noreply.github.com>
2020-05-15 23:36:32 -0400

    Update loops.py

--- Assignment2/loops.py
+++ Assignment2/loops.py
@@ -1,3 +1,6 @@
+#-------------------------------------------------------------
+#                        Decrypting Q
+#-------------------------------------------------------------
 FROM = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ,.'"
 TO = "I7AOwhLWeKvj8qNksRxEi UpfQm4BP9FXZy'aHr162TY5o3tnVuCG,lgbz0M.SDcJd"
 REVERSE_SUBSTITUTION = ''.maketrans(TO, FROM)
@@ -6,10 +9,16 @@ def decrypt(message, substitution_table):
   """Decrypts the message once using the given translation table."""
   return message.translate(substitution_table)
 
-# The answer to question 1 has been encrypted. 
+
+
+# The answer to question 1 has been encrypted.
 message = ".KrsUIfRrcsQJsy8rfQMJIsdsMfsGW8WWMrfGN"
 
 # This decrypts the message once, you'll need to do this 100 times in a loop to reveal the answer
-message = decrypt(message, REVERSE_SUBSTITUTION)
 
-print(message)
+for i in range (100):
+  message = decrypt(message, REVERSE_SUBSTITUTION)
+
+  print(message)
+
+#-------------------------------------------------------------
